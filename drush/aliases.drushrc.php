8c0b1b4bf1dec30b300f46d7b5c846ac36b663ca
Arto Iijalainen <arto.iijalainen@gmail.com>
2018-06-26 13:07:31 +0300

    Updated drush site aliases.

--- drush/aliases.drushrc.php
+++ drush/aliases.drushrc.php
@@ -6,29 +6,14 @@
  * @link https://github.com/drush-ops/drush/blob/master/examples/example.aliases.drushrc.php
  */
 
-/**
- * Placeholder aliases. Remove the leading hash signs to enable.
- *
- * These are the environment names we have decided to use, please don't change
- * them.
- */
-#$aliases['production'] = array(
-#  'uri' => 'http://example.com',
-#  'root' => '/var/www/example.com/current',
-#  'remote-host' => 'example.com',
-#  'remote-user' => 'root',
-#);
+$aliases['production'] = [
+  'uri' => 'https://datasupport.helsinki.fi',
+  'root' => '/data/rds/public',
+  'remote-host' => 'datasupport.helsinki.fi',
+];
 
-#$aliases['staging'] = array(
-#  'uri' => 'http://staging.example.com',
-#  'root' => '/var/www/example.com/current',
-#  'remote-host' => 'staging.example.com',
-#  'remote-user' => 'root',
-#);
-
-$aliases['testing'] = array(
-  'uri' => 'https://dev.researchdata-hy.com/',
-  'root' => '/var/www/dev.researchdata-hy.com/public',
-  'remote-host' => 'dev.researchdata-hy.com',
-  'remote-user' => 'centos',
-);
+$aliases['testing'] = [
+  'uri' => 'https://datasupport-test.it.helsinki.fi',
+  'root' => '/data/rds/public',
+  'remote-host' => 'datasupport-test.it.helsinki.fi',
+];
