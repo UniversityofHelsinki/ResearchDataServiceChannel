63b108685833e7a86194d259db9d57da5ac39329
Marko Korhonen <marko.korhonen@druid.fi>
2019-09-30 11:38:01 +0300

    Simple aliases and with custom username (prompted)

--- tools/make/project/project.mk
+++ tools/make/project/project.mk
@@ -3,6 +3,8 @@ CLI_SHELL := bash
 CLI_USER := drupal
 DOCKER_PROJECT_ROOT := /var/www/drupal/public_html
 
+SYNC_TARGETS := drush-sync-prompt-for-username
+
 ifeq ($(ENV),production)
 	DRUPAL_POST_INSTALL_TARGETS := drush-updb drush-cr drush-uli
 else
@@ -13,3 +15,10 @@ PHONY += drush-filestage
 drush-filestage:
 	$(call step,Enable stage_file_proxy module...)
 	$(call drush_on_${RUN_ON},en -y stage_file_proxy)
+
+drush-sync-prompt-for-username: PROMPT := Give your HY username for syncing data:
+drush-sync-prompt-for-username:
+	$(call step,Sync database from @$(DRUPAL_SYNC_SOURCE)...)
+	@${DOCKER_COMPOSE_EXEC} --env PROJECT_SSH_USER=$(shell bash -c 'read -p "$(PROMPT) " u; echo $$u') \
+		-u ${CLI_USER} ${CLI_SERVICE} ${CLI_SHELL} \
+		-c "drush --ansi --strict=0 sql-sync -y @$(DRUPAL_SYNC_SOURCE) @self"
