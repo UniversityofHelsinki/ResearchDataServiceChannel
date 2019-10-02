55b0c37ccc1137224560dae49ee4b85d4c8f8639
Marko Korhonen <marko.korhonen@druid.fi>
2019-10-02 13:48:21 +0300

    Fix deprecation

--- public/modules/custom/hy_samlauth/src/EventSubscriber/RedirectAnonymousSubscriber.php
+++ public/modules/custom/hy_samlauth/src/EventSubscriber/RedirectAnonymousSubscriber.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\hy_samlauth\EventSubscriber;
 
+use Drupal;
+use Drupal\Core\Url;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
@@ -31,20 +33,19 @@ class RedirectAnonymousSubscriber implements EventSubscriberInterface {
    *
    */
   public function checkAuthStatus(GetResponseEvent $event) {
-
     // Is current path correct path?
-    $current_path = \Drupal::url('<current>', [], ['absolute' => FALSE]);
+    $current_path = Url::fromRoute('<current>', [], ['absolute' => FALSE]);
     if (strpos($current_path, '/service-order-form') !== 0) {
       return;
     }
 
     // Check if user has logged in already.
-    $tempstore = \Drupal::service('user.private_tempstore')->get('hy_samlauth');
+    $tempstore = Drupal::service('user.private_tempstore')->get('hy_samlauth');
     $user = $tempstore->get(SamlService::SESSION_SAML_USER);
     if (empty($user)) {
       $response = new RedirectResponse('/saml/login', 301);
 
-      $cookie_saml_redirect = new Cookie(SamlService::COOKIE_SAML_REDIRECT, serialize(\Drupal::request()->getRequestUri()));
+      $cookie_saml_redirect = new Cookie(SamlService::COOKIE_SAML_REDIRECT, serialize(Drupal::request()->getRequestUri()));
       $response->headers->setCookie($cookie_saml_redirect);
       $event->setResponse($response);
       $event->stopPropagation();
