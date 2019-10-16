3f6eafc849e649e859a3f14aaedca66aebaa2ed7
Marko Korhonen <marko.korhonen@druid.fi>
2019-10-16 13:38:39 +0300

    Use latest images

--- tools/make/project/project.mk
+++ tools/make/project/project.mk
@@ -1,3 +1,9 @@
+PROMPT := Give your HY username for syncing data:
+shell: --shell-prompt
+
+--shell-prompt:
+	$(call docker_run_cmd,grep -qF 'export PROJECT_SSH_USER' ~/.bashrc || echo 'export PROJECT_SSH_USER=$(shell bash -c 'read -p "$(PROMPT) " u; echo $$u')' >> ~/.bashrc)
+
 SYNC_TARGETS := drush-sync-prompt-for-username
 
 ifeq ($(ENV),production)
@@ -11,9 +17,8 @@ drush-filestage:
 	$(call step,Enable stage_file_proxy module...)
 	$(call drush_on_${RUN_ON},en -y stage_file_proxy)
 
-drush-sync-prompt-for-username: PROMPT := Give your HY username for syncing data:
 drush-sync-prompt-for-username:
 	$(call step,Sync database from @$(DRUPAL_SYNC_SOURCE)...)
-	@${DOCKER_COMPOSE_EXEC} --env PROJECT_SSH_USER=$(shell bash -c 'read -p "$(PROMPT) " u; echo $$u') \
+	@docker-compose exec --env PROJECT_SSH_USER=$(shell bash -c 'read -p "$(PROMPT) " u; echo $$u') \
 		-u ${CLI_USER} ${CLI_SERVICE} ${CLI_SHELL} \
 		-c "drush --ansi --strict=0 sql-sync -y @$(DRUPAL_SYNC_SOURCE) @self"
